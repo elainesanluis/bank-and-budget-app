09617ed9ffb4fa92286e92e2afbb6235e9d4cf0e
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-09-29 20:33:53 +0800

    updated code in AccountsPage and CreateAccountsPage

--- src/Pages/AccountsPage.js
+++ src/Pages/AccountsPage.js
@@ -3,20 +3,44 @@ import React from 'react';
 import './AccountsPage.css';
 import { Link } from 'react-router-dom';
 
-function AccountsPage() {
+function AccountsPage({ accountDetails }) {
   return (
     <div>
-    <div id='create-account-logo'>
-    <Link to="/">
-        <img className='bankLogo' src='https://freeiconshop.com/wp-content/uploads/edd/bank-flat.png' alt='Bank Logo' />
-      </Link>
-      <h1>Bank of Avion School of the Philippines</h1>
-    </div>
-    <br/>
-    <br/>
-    <br/>
-        
+      <div id='create-account-logo'>
+        <Link to="/">
+          <img className='bankLogo' src='https://freeiconshop.com/wp-content/uploads/edd/bank-flat.png' alt='Bank Logo' />
+        </Link>
+        <h1>Bank of Avion School of the Philippines</h1>
+      </div>
+      <br />
+      <br />
+      <br />
+
       <h2>Accounts Page</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Deposit Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(accountDetails) && accountDetails.length > 0 ? (
+            accountDetails.map((account, index) => (
+              <tr key={index}>
+                <td>{account.firstName}</td>
+                <td>{account.lastName}</td>
+                <td>{account.client1Balance}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3">No accounts to display</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/Pages/CreateAccountPage.js
+++ src/Pages/CreateAccountPage.js
@@ -1,8 +1,36 @@
 // AccountsPage.js
-import React from 'react';
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function CreateAccountPage() {
+     // Initialize state to hold submitted account details
+  const [accountDetails, setAccountDetails] = useState({});
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [client1Balance, setClient1Balance] = useState('');
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit button clicked');
+    // Create an object with the submitted data
+    const newAccount = {
+      firstName,
+      lastName,
+      client1Balance
+    };
+    console.log('New account data:', newAccount);
+
+    // Add the new account to the state
+    setAccountDetails(newAccount);
+
+    // Clear the form inputs
+    setFirstName('');
+    setLastName('');
+    setClient1Balance('');
+  };
+
   return (
     <div>
     <div id='create-account-logo'>
@@ -17,10 +45,10 @@ function CreateAccountPage() {
         
     <div id='create-account-form'>
       <h2>Create Account</h2>
-    <form>
-        <input id='firstName' type='text' name='firstName' placeholder='First Name' pattern='[A-Za-z ]+' required/> <br />
-        <input id='lastName' type='text' name='lastName' placeholder='Last Name' pattern='[A-Za-z ]+' required/> <br />
-        <input id='client1Balance' type='number' name='client1Balance' placeholder='Deposit Amount' min='500.00' pattern='^[0-9]+(\.[0-9]{2})?$' required/> <br />
+    <form onSubmit={handleSubmit}>
+        <input id='firstName' type='text' name='firstName' placeholder='First Name' pattern='[A-Za-z ]+' required onChange={(e) => setFirstName(e.target.value)}/> <br />
+        <input id='lastName' type='text' name='lastName' placeholder='Last Name' pattern='[A-Za-z ]+' required onChange={(e) => setLastName(e.target.value)}/> <br />
+        <input id='client1Balance' type='number' name='client1Balance' placeholder='Deposit Amount' min='500.00' pattern='^[0-9]+(\.[0-9]{2})?$' required onChange={(e) => setClient1Balance(e.target.value)}/> <br />
         <input id='submitAccount' type='submit' name='submit' value='Submit'/>
     </form>
     </div>
